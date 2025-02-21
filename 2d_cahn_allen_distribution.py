ce3c4e976b4c8509acb201a08ad428b7311b8000
llautwein <lucas-lautwein@t-online.de>
2025-02-21 23:58:41 +0000

    Added a config file to summarise constants and ICs in one place

--- 2d_cahn_allen_distribution.py
+++ 2d_cahn_allen_distribution.py
@@ -3,53 +3,36 @@ from dolfin import *
 import ufl
 import numpy as np
 import matplotlib.pyplot as plt
+import config as cfg
 
-# Parameters
-T = 10  # Time interval length
-num_steps = 50  # number of time steps
-dt = T / num_steps
-mu = 0.0  # moisture parameter
-L = 10  # domain length
-eps = -1e-1  # how much the ink particles like to be in the solvent
-sigma = 0.01
-M = 0.1
-M_sig = M * sigma  # product of evaporation coefficient and surface tension
-# appears through non-dimensionalisation
+# available phi ic options: constant, tanh, cosine, gaussian, sine_checkerboard, random
+phi_init_option = "gaussian"
 
-# mesh and function space (linear Lagrange elements and 1d interval)
-nx = 50
-ny = 50
-mesh = RectangleMesh(Point(0, 0), Point(L, L), nx, ny)
-V = FunctionSpace(mesh, "Lagrange", 1)
+# available n ic options: constant, gaussian, half_domain
+n_init_option = "gaussian"
 
 # mobility function
 def D(p):
-    return 0.5*ufl.tanh(4*phi)+0.5
+    return 0.05*(1+ufl.tanh(20*phi))
 
-# initial conditions
-#phi_k = interpolate(Expression("0.2*tanh(x[0]-5)", degree=2), V)
-#phi_k = interpolate(Expression("0.2*cos(pi*(x[0]+x[1])/5)", degree=2), V)
-phi_k = Function(V)
-vec = phi_k.vector()
-vec[:] = np.random.uniform(-0.1, 0.1, phi_k.vector()[:].size)
+config = cfg.Config()
+
+phi_k, n_k = config.set_ics(phi_init_option, n_init_option)
 phi_0 = phi_k.copy(deepcopy=True)
-
-n_k = interpolate(Constant(1/L), V)
-# n_k = interpolate(Expression("x[0] < L/2 ? 0.2 : 0", L=L, degree=1), V)
-#n_k = interpolate(Expression("0.1*exp(-pow(x[0]-L/2, 2)/0.01)", L=L, degree=2), V)
 n_0 = n_k.copy(deepcopy=True)
 
 # weak formulation
-phi = Function(V)
-v = TestFunction(V)
-F_phi = (phi - phi_k) / dt * v * dx + inner(grad(phi), grad(v)) * dx - (-phi ** 3 + phi - eps * n_k + mu) * v * dx
+phi = Function(config.V)
+v = TestFunction(config.V)
+F_phi = (phi - phi_k) / config.dt * v * dx + inner(grad(phi), grad(v)) * dx + (phi ** 3 - phi + config.eps * n_k - config.mu) * v * dx
 J_phi = derivative(F_phi, phi)
 
-n = Function(V)
-F_n = (n - n_k) / dt * v * dx + (1 / M_sig) * D(phi) * inner(grad(n) + eps * n * grad(phi), grad(v)) * dx
+n = Function(config.V)
+F_n = (n - n_k) / config.dt * v * dx + (1 / config.M_sig) * D(phi) * inner(grad(n) + config.eps * n * grad(phi), grad(v)) * dx
 J_n = derivative(F_n, n)
 
-free_energy = (-(phi**2)/2 + (phi**4)/4 + sigma/2*inner(grad(phi), grad(phi)) - mu*phi)*dx
+free_energy = (-(phi**2)/2 + (phi**4)/4 + config.sigma/2*inner(grad(phi), grad(phi)) -
+               config.mu*phi + n*(ufl.ln(n)-1) + config.eps*n*phi)*dx
 
 # set up of the solvers
 problem_phi = NonlinearVariationalProblem(F_phi, phi, J=J_phi)
@@ -72,8 +55,8 @@ n_xdmf.write(n_k, 0)
 iterates_phi = [phi_0]
 iterates_n = [n_0]
 free_energy_vals = []
-for i in range(num_steps):
-    t = (i+1)*dt
+for i in range(config.num_steps):
+    t = (i+1)*config.dt
     solver_phi.solve()
     phi_k.assign(phi)
     iterates_phi.append(phi_k.copy(deepcopy=True))
@@ -90,12 +73,9 @@ plt.rcParams.update({'font.size': 18})
 fig, axes = plt.subplots(2, 2, figsize=(16, 12), constrained_layout=True)
 cmap = "coolwarm"
 
-vmin = phi.vector().min()
-vmax = phi.vector().max()
-
 plt.sca(axes[0, 0])
 c0 = plot(phi_0, mode="color")
-c0.set_clim(vmin, vmax)
+c0.set_clim(-1, 1)
 c0.set_cmap(cmap)
 axes[0, 0].set_title("Initial condition (order parameter)")
 axes[0, 0].set_xlabel("x")
@@ -103,9 +83,9 @@ axes[0, 0].set_ylabel("y")
 
 plt.sca(axes[0, 1])
 c1 = plot(phi, mode="color")
-c1.set_clim(vmin, vmax)
+c1.set_clim(-1, 1)
 c1.set_cmap(cmap)
-axes[0, 1].set_title(f"Solution at t={T} (order parameter)")
+axes[0, 1].set_title(f"Solution at t={config.T} (order parameter)")
 axes[0, 1].set_xlabel("x")
 axes[0, 1].set_ylabel("y")
 
@@ -114,8 +94,8 @@ cbar1.set_label("Order parameter")
 
 #########################################
 cmap = "binary"
-nmin = n.vector().min()
-nmax = n.vector().max()
+nmin = n_0.vector().min()
+nmax = n_0.vector().max()
 
 plt.sca(axes[1, 0])
 c2 = plot(n_0, mode="color")
@@ -129,7 +109,7 @@ plt.sca(axes[1, 1])
 c3 = plot(n, mode="color")
 c3.set_clim(nmin, nmax)
 c3.set_cmap(cmap)
-axes[1, 1].set_title(f"Solution at t={T} (distribution)")
+axes[1, 1].set_title(f"Solution at t={config.T} (distribution)")
 axes[1, 1].set_xlabel("x")
 axes[1, 1].set_ylabel("y")
 
@@ -140,7 +120,7 @@ plt.show()
 plt.close(fig)
 #################################
 plt.figure(figsize=(12, 6))
-plt.plot([dt*k for k in range(num_steps)], free_energy_vals)
+plt.plot([config.dt*k for k in range(config.num_steps)], free_energy_vals)
 plt.xlabel("Time")
 plt.ylabel("Free energy")
 plt.title("Evolution of free energy over time")
---
+++ config.py
@@ -0,0 +1,50 @@
+from dolfin import *
+import fenics
+import numpy as np
+
+
+class Config:
+
+    def __init__(self):
+        self.T = 10  # Time interval length
+        self.num_steps = 50  # number of time steps
+        self.dt = self.T / self.num_steps
+        self.mu = 0  # moisture parameter
+        self.L = 25  # domain length
+        self.eps = -1  # how much the ink particles like to be in the solvent
+        self.sigma = 0.03
+        self.M = 0.2
+        self.M_sig = self.M * self.sigma
+
+        self.nx, self.ny = 100, 100
+        self.mesh = RectangleMesh(Point(0, 0), Point(self.L, self.L), self.nx, self.ny)
+        self.V = FunctionSpace(self.mesh, "Lagrange", 1)
+
+        self.phi_options = {
+            "constant": Constant(-1),
+            "tanh":  Expression("0.2*tanh(x[0]-5)", degree=2),
+            "cosine": Expression("0.2*cos(pi*(x[0]+x[1])/5)", degree=2),
+            "gaussian": Expression("2 * exp((-pow(x[0]-L/2,2) - pow(x[1]-L/2,2))/10)-1", L=self.L, degree=2),
+            "sine_checkerboard": Expression("sin(x[0])*sin(x[1])", degree=2),
+            "random": self.random_phi()
+        }
+
+        self.n_options = {
+            "constant": Constant(0),
+            "gaussian": Expression("2 * exp((-pow(x[0]-L/2,2) - pow(x[1]-L/2,2))/10)", L=self.L, degree=2),
+            "half_domain": Expression("x[0] < L/2 ? 0.2 : 0", L=self.L, degree=1)
+        }
+
+    def set_ics(self, phi_option, n_option):
+        if phi_option == "random":
+            phi_init = self.phi_options[phi_option]
+        else:
+            phi_init = interpolate(self.phi_options[phi_option], self.V)
+        n_init = interpolate(self.n_options[n_option], self.V)
+        return phi_init, n_init
+
+    def random_phi(self):
+        phi_k = Function(self.V)
+        vec = phi_k.vector()
+        vec[:] = np.random.uniform(-0.1, 0.1, phi_k.vector()[:].size)
+        return phi_k
